d1751ae78aeca37e09bc2846135658943ec8a15e
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-03-17 23:49:06 +0530

    Added Program Description

--- friend_function.cpp
+++ friend_function.cpp
@@ -1,36 +1,34 @@
+========================================================================================================================================
+//This is a program for friend function 
+========================================================================================================================================
+
 #include <iostream>
- 
 using namespace std;
  
-class Box {
+class Box 
+{
    double width;
-   
    public:
-      friend void printWidth( Box box );
-      void setWidth( double wid );
+      friend void Print_width( Box box );
+      void Set_width( double Entered_wid );
 };
 
-// Member function definition
-void Box::setWidth( double wid ) {
-   width = wid;
+
+void Box::setWidth( double Entered_wid )
+{
+   width = Entered_wid;
 }
 
-// Note: printWidth() is not a member function of any class.
-void printWidth( Box box ) {
-   /* Because printWidth() is a friend of Box, it can
-   directly access any member of this class */
+void Print_width( Box box ) 
+{
    cout << "Width of box : " << box.width <<endl;
 }
  
-// Main function for the program
-int main() {
+
+int main() 
+{
    Box box;
- 
-   // set box width without member function
-   box.setWidth(10.0);
-   
-   // Use friend function to print the wdith.
-   printWidth( box );
- 
+   box.Set_width(10.0);
+   Print_width( box );
    return 0;
 }
