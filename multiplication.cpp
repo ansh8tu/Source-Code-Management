43c11c946634be42247589270de0159c5da8f2b5
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-03-19 02:46:15 +0530

    Added Program description

--- multiplication.cpp
+++ multiplication.cpp
@@ -1,18 +1,15 @@
+===========================================================================================================================================
+//This is a program for multiplying two numbers    
+===========================================================================================================================================
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    double firstNumber, secondNumber, productOfTwoNumbers;
-    cout << "Enter two numbers: ";
-
-    // Stores two floating point numbers in variable firstNumber and secondNumber respectively
+    double First_number, Second_number, Product_of_two_numbers;
+    cout << "Enter any two numbers that you wish to multiply: ";
     cin >> firstNumber >> secondNumber;
- 
-    // Performs multiplication and stores the result in variable productOfTwoNumbers
-    productOfTwoNumbers = firstNumber * secondNumber;  
-
-    cout << "Product = " << productOfTwoNumbers;    
-    
+    Product_of_two_numbers = First_number * Second_number;  
+    cout << "Product of "<<First_number<<"and "<<Second_number<<"is = " << Product_of_two_numbers;    
     return 0;
 }
