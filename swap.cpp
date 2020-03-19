13f9b1f542a8927fdc336c70a3617e90a7ffc9d3
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-03-20 01:37:55 +0530

    Added Program Description

--- swap.cpp
+++ swap.cpp
@@ -1,19 +1,22 @@
+===========================================================================================================================================
+//This is a tprogram to swap two numbers    
+===========================================================================================================================================
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int a = 5, b = 10, temp;
+    int Number_1 = 5, Number_2 = 10, temporary;
 
     cout << "Before swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
+    cout << "Number_1 = " << Number_1 << ", Number_2 = " << Number_2 << endl;
 
-    temp = a;
-    a = b;
-    b = temp;
+    temp = Number_1;
+    Number_1 = Number_2;
+    Number_2 = temp;
 
     cout << "\nAfter swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
+    cout << "Number_1 = " << Number_1 << ", Number_2 = " << Number_2 << endl;
 
     return 0;
 }
