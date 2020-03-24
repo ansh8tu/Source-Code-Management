8e6f6cfa9133669fb3319fb3b9f805e4437e3666
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-03-24 15:52:35 +0530

    Added Program description

--- Multidimensional_array.cpp
+++ Multidimensional_array.cpp
@@ -1,39 +1,33 @@
+========================================================================================================================================
+//This is a C++ program for a three dimensional array.	
+========================================================================================================================================
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    // This array can store upto 12 elements (2x3x2)
-    int test[2][3][2];
-
+    int arr[2][3][2];
     cout << "Enter 12 values: \n";
-    
-    // Inserting the values into the test array
-    // using 3 nested for loops.
     for(int i = 0; i < 2; ++i)
     {
         for (int j = 0; j < 3; ++j)
         {
             for(int k = 0; k < 2; ++k )
             {
-                cin >> test[i][j][k];
+                cin >> arr[i][j][k];
             }
         }
     }
-
     cout<<"\nDisplaying Value stored:"<<endl;
-
-    // Displaying the values with proper index.
     for(int i = 0; i < 2; ++i)
     {
         for (int j = 0; j < 3; ++j)
         {
             for(int k = 0; k < 2; ++k)
             {
-                cout << "test[" << i << "][" << j << "][" << k << "] = " << test[i][j][k] << endl;
+                cout << "arr[" << i << "][" << j << "][" << k << "] = " << arr[i][j][k] << endl;
             }
         }
     }
-
     return 0;
 }
