f75b15367ef0f36507a517e86dc8f3edaafe516b
Anshuman Mishra <55720064+ansh8tu@users.noreply.github.com>
2020-05-18 03:00:37 +0530

    Create delete_ptr.cpp

---
+++ delete_ptr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+#include <cstdlib>  
+using namespace std;  
+  
+int main()  
+{  
+ int *ptr;  
+ ptr=new int;  
+ *ptr=68;  
+ std::cout << "The value of p is : " <<*ptr<< std::endl;  
+ delete ptr;  
+ std::cout <<"The value after delete is : "  <<*ptr<< std::endl;  
+ return 0;  
+}
